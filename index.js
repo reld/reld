f2cffebc9cda121a3dfa911560288c96fbd60ef1
Rómulo Lazarde <rl@iconstorm.com>
2021-09-07 12:53:18 +0200

    Adds short bio

--- index.js
+++ index.js
@@ -1,2 +1,36 @@
 #!/usr/bin/env node
-console.log('Hello!');
+console.log(`
+Rómulo E. Lazarde Delgado
+Creative Techonologist
+
+Skills:
+- Fast Prototyping Soft- / Hardware
+- Design Systems - Pipeline / Components creation, setup and maintainance
+- Fullstack development
+- Research and innovation
+- DevOps - AWS, Azure, Linux, NginX, Docker
+
+Languages:
+- Javascript
+- CSS
+- HTML
+- Python
+- PHP
+- Bash
+- Typescript
+
+Frameworks:
+- Svelte
+- Vue
+- React
+- Angular
+- Preact
+
+Www:
+- https://iconstorm.com
+
+Code: 
+- https://github.com/reld
+- https://github.com/iconstorm
+
+`);
